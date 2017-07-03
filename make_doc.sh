5310e203c363e79ad1642bc40f18328b104673aa
Rikard Fröberg <rikard@morus.se>
2017-07-03 02:09:05 +0200

    New destination

--- make_doc.sh
+++ make_doc.sh
@@ -3,4 +3,4 @@
 JAVA_VERSION=8
 PACKAGES="se.itu.hunt"
 
-javadoc --source-path src/ -d doc -link http://docs.oracle.com/javase/${JAVA_VERSION}/docs/api/ $PACKAGES 
+javadoc --source-path src/ -d docs -link http://docs.oracle.com/javase/${JAVA_VERSION}/docs/api/ $PACKAGES 
